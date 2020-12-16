0194e3311fb557bd700e56ba345950af766f1637
thomas-li-sjtu <980226547@qq.com>
2020-12-16 14:13:57 +0800

    Reformat and add comments to Apply

--- apply.py
+++ apply.py
@@ -7,30 +7,51 @@ MAX_LEN = 16
 CHARMAP = './charmap.pickle'
 TERMINAL_SYMBOL = False
 ENCODING = 'ascii'
-#---------------------------
+# ---------------------------
 
 from inference import Inference
 import myPickle, sys
 import tensorflow as tf
 
+
 def read_passwords(path, encoding=ENCODING, MIN_LEN=0, MAX_LEN=16):
+    """
+    从文件读入口令
+    Args:
+        path: 文件路径
+        encoding: 编码
+        MIN_LEN: 口令最短长度
+        MAX_LEN: 口令最长长度
+
+    Returns: 口令列表
+    """
     X = []
     with open(path, encoding=encoding, errors='ignore') as f:
         for x in f:
-            x = x[:-1]
-            if len(x) <= MAX_LEN and len(x) >= MIN_LEN:
+            x = x[:-1]  # 除去换行符
+            if MAX_LEN >= len(x) >= MIN_LEN:
                 X.append(x)
     return X
 
-def write_tsv(output, X, P, encoding=ENCODING):
-    assert len(X) == len(P)
-    n = len(X)
+
+def write_tsv(output, passwords, log_probability, encoding=ENCODING):
+    """
+    输出结果到文件中
+    Args:
+        output: 输出路径
+        passwords: 口令列表
+        log_probability: 非归一化的概率列表(log)
+        encoding: 编码
+
+    Returns:
+    """
+    assert len(passwords) == len(log_probability)
+    n = len(passwords)
     with open(output, 'w', encoding=encoding) as f:
-        for x, p in zip(X, P):
+        for x, p in zip(passwords, log_probability):
             print("%s\t%f" % (x, p), file=f)
-            
-            
-            
+
+
 if __name__ == '__main__':
     try:
         model_path = sys.argv[1]
@@ -39,13 +60,12 @@ if __name__ == '__main__':
     except:
         print("USAGE: model_path.h5 password_path.txt output_path.txt")
         sys.exit(1)
-    
-    
-    X = read_passwords(password_file)
-    cm = myPickle.load(CHARMAP)
+
+    passwords = read_passwords(password_file)
+    charmap = myPickle.load(CHARMAP)
 
     model = tf.keras.models.load_model(model_path, compile=False)
-    S = Inference(model, cm, MAX_LEN, BATCH_SIZE)
-    
-    logP = S.applyBatch(X, TERMINAL_SYMBOL)
-    write_tsv(output_path, X, logP)
+    infer = Inference(model, charmap, MAX_LEN, BATCH_SIZE)
+
+    logP = infer.applyBatch(passwords, TERMINAL_SYMBOL)  # 计算概率
+    write_tsv(output_path, passwords, logP)
